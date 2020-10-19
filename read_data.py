63d6260878bde9786bb7b540e5628afee5960404
yiheng123 <yiheng@nutonomy.com>
2020-10-20 04:05:20 +0800

    haha

--- read_data.py
+++ read_data.py
@@ -1,8 +1,9 @@
 # read data return as dictionary
 # parameter
-DATA_FILE = "testdata.txt"
-OUTPUT_FILE = "output.txt"
-SAVE_FILE = "savedata.txt"
+DATA_FILE = "./data/testdata.txt"
+OUTPUT_FILE = "./data/output.txt"
+SAVE_FILE = "./data/savedata.txt"
+
 
 def graph_reader():
     nodeDict={}
